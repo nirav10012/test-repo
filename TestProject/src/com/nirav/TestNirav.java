7bf3fbebd8eb1c999725e84f184afa49d6eaa496
npatel <npatel@SFOWLP-2DM2QQ2.ad.businesswire.com>
2018-08-18 22:05:35 -0700

    Code cleanup

--- TestProject/src/com/nirav/TestNirav.java
+++ TestProject/src/com/nirav/TestNirav.java
@@ -1,19 +1,13 @@
 package com.nirav;
+
 /*Git Test method*/
 public class TestNirav {
 
 	public static void main(String[] args) {
-
 		System.out.println(testMthodMultiplication(3, 3.0f));
-		//System.out.println(testMthodDivision(9, 3.0f));
-
-
 	}
 
-	
-	
 	public static int testMthodMultiplication(int x, float f2) {
 		return Integer.valueOf(x * (int) f2 * 4);
-//
 	}
 }
