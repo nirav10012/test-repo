da50cf4bc668e06e66684594466b8ca1fb77e14c
npatel <npatel@SFOWLP-2DM2QQ2.ad.businesswire.com>
2018-08-18 21:36:10 -0700

    Support for multi method

--- TestProject/src/com/nirav/TestNirav.java
+++ TestProject/src/com/nirav/TestNirav.java
@@ -4,10 +4,15 @@ public class TestNirav {
 
 	public static void main(String[] args) {
 		System.out.println(testMthod(3, 3.0f));
-
+		System.out.println(testMthodMultiplication(2, 9.2f));
 	}
 
 	public static int testMthod(int x, float f2) {
 		return Integer.valueOf(x * (int) f2);
+	}
+	
+	
+	public static int testMthodMultiplication(int x, float f2) {
+		return Integer.valueOf(x * (int) f2 * 4);
 	}
 }
