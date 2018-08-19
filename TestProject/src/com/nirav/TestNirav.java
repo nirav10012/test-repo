af012cbc8560bfa4396f43c2a9adc197d79c03fa
npatel <npatel@SFOWLP-2DM2QQ2.ad.businesswire.com>
2018-08-18 21:13:41 -0700

    initial commit..

---
+++ TestProject/src/com/nirav/TestNirav.java
@@ -0,0 +1,13 @@
+package com.nirav;
+/*Git Test method*/
+public class TestNirav {
+
+	public static void main(String[] args) {
+		System.out.println(testMthod(3, 3.0f));
+
+	}
+
+	public static int testMthod(int x, float f2) {
+		return Integer.valueOf(x * (int) f2);
+	}
+}
