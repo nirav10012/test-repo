62b6eb556896eb7080219936ebfd951eec5d5241
nirav10012 <nirav10012@gmail.com>
2018-08-28 14:34:17 -0700

    Update TestNirav.java
    Changes commiting in git for check in eclipse.

--- TestProject/src/com/nirav/TestNirav.java
+++ TestProject/src/com/nirav/TestNirav.java
@@ -5,6 +5,8 @@ public class TestNirav {
 
 	public static void main(String[] args) {
 		System.out.println(testMthodMultiplication(3, 3.0f));
+		System.out.println("Changes");
+
 	}
 
 	public static int testMthodMultiplication(int x, float f2) {
