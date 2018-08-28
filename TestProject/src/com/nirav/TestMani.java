4a4fb66831aec184ed85b6271eaf570537a7d60d
nirav10012 <nirav10012@gmail.com>
2018-08-28 15:42:35 -0700

    Update TestMani.java
    Update in git

--- TestProject/src/com/nirav/TestMani.java
+++ TestProject/src/com/nirav/TestMani.java
@@ -4,7 +4,7 @@ Purpose: Git demo changing branch in to master */
 public class TestMani{
 
 private java.util.Date getGMTTime(){
-  //Edited page for eclipse local branch
+  //Edited page for eclipse local branch in git update
   return new java.util.Date();
 }
 
